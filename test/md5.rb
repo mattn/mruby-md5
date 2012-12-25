62edfa6f30688ebe2afe59196bdb1c88ab56618a
mattn <mattn.jp@gmail.com>
2012-12-25 21:58:38 +0900

    fix test.

--- test/md5.rb
+++ test/md5.rb
@@ -1,5 +1,5 @@
 assert('MD5 Hash for "ruby"') do
-  MD5.md5_hex('ruby') == '58e53d1324eef6265fdb97b08ed9aadf'
+  MD5::md5_hex('ruby') == '58e53d1324eef6265fdb97b08ed9aadf'
 end
 
 assert('MD5 Hash for old example') do
